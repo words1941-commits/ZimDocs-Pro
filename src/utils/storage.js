4fa1a427d2cb698b92ee3320f5873ca23ee9cf03
words1941-commits <words1941@gmail.com>
2025-11-10 17:15:19 +0200

    Delete src directory

--- src/utils/storage.js
+++
@@ -1,7 +0,0 @@
-import localforage from 'localforage'
-localforage.config({ name: 'zimdocs' })
-export default {
-  async save(k,v){ return localforage.setItem(k,v) },
-  async get(k){ return localforage.getItem(k) },
-  async remove(k){ return localforage.removeItem(k) }
-}
