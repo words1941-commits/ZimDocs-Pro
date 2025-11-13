977e14edb37d8c4254cde6848b3de130cad5558a
words1941-commits <words1941@gmail.com>
2025-11-13 18:11:08 +0200

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,41 +1,31 @@
-const CACHE_NAME = "zimdocs-pro-v9";
-const OFFLINE_URL = "./index.html";
-
-const FILES_TO_CACHE = [
+const CACHE = "zimdocs-pro-v5";
+const FILES = [
   "./",
   "./index.html",
   "./manifest.json",
   "./css/style.css",
-  "./js/app.js",
-  "./lib/jspdf.umd.min.js"
+  "./js/app.js"
 ];
 
-// Install
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
 
-// Activate
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key))))
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
   );
   self.clients.claim();
 });
 
-// Fetch
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
-        .then(networkResponse => {
-          if(event.request.url.startsWith("http")){
-            caches.open(CACHE_NAME).then(cache => cache.put(event.request, networkResponse.clone()));
-          }
-          return networkResponse;
-        })
-        .catch(()=>caches.match(OFFLINE_URL));
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (e.request.url.startsWith("http")) {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+      }
+      return res;
+    }))
   );
 });
