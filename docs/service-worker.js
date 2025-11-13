c655b558db665e5e88c32aeb615177b396ac1a4f
words1941-commits <words1941@gmail.com>
2025-11-13 12:57:53 +0200

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,35 +1,64 @@
-const CACHE_NAME = 'zimdocs-pro-v3';
+const CACHE_NAME = "zimdocs-pro-v4";
+const OFFLINE_URL = "./index.html";
+
 const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './css/styles.css',
-  './js/app.js',
-  './icons/icon-192x192.png',
-  './icons/icon-512x512.png',
-  './icons/maskable.png',
-  './icons/screenshot1.png',
-  './icons/screenshot2.png'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./js/app.js",
+  "./assets/markable.png",
+  "./assets/screenshot1.png",
+  "./assets/screenshot2.png"
 ];
 
-self.addEventListener('install', event => {
+// Install
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+// Activate
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => key !== CACHE_NAME && caches.delete(key))
-    ))
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
+// Fetch handler
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request)
+          .then((networkResponse) => {
+            if (event.request.url.startsWith("http")) {
+              caches.open(CACHE_NAME).then((cache) =>
+                cache.put(event.request, networkResponse.clone())
+              );
+            }
+            return networkResponse;
+          })
+          .catch(() => caches.match(OFFLINE_URL))
+      );
+    })
   );
 });
+
+// Background sync placeholder
+self.addEventListener("sync", (event) => {
+  if (event.tag === "sync-zimdocs-data") {
+    event.waitUntil(syncOfflineData());
+  }
+});
+
+async function syncOfflineData() {
+  const data = await self.clients.matchAll();
+  console.log("Syncing offline data", data.length, "clients.");
+}
