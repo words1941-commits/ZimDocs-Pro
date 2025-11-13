5e0a889b07914b1cd82d54292d48092a5ebffaff
words1941-commits <words1941@gmail.com>
2025-11-13 17:13:27 +0200

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,33 +1,41 @@
-const CACHE_NAME = "zimdocs-pro-v5";
-const OFFLINE_URL = "/ZimDocs-Pro/docs/index.html";
+const CACHE_NAME = "zimdocs-pro-v9";
+const OFFLINE_URL = "./index.html";
 
 const FILES_TO_CACHE = [
-  "/ZimDocs-Pro/docs/",
-  "/ZimDocs-Pro/docs/index.html",
-  "/ZimDocs-Pro/docs/manifest.json",
-  "/ZimDocs-Pro/docs/css/style.css",
-  "/ZimDocs-Pro/docs/js/app.js",
-  "/ZimDocs-Pro/docs/assets/icons/icon-192x192.png",
-  "/ZimDocs-Pro/docs/assets/icons/icon-384x384.png",
-  "/ZimDocs-Pro/docs/assets/icons/icon-512x512.png",
-  "/ZimDocs-Pro/docs/assets/screenshot1.png",
-  "/ZimDocs-Pro/docs/assets/screenshot2.png"
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./js/app.js",
+  "./lib/jspdf.umd.min.js"
 ];
 
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
+// Activate
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => 
-    Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-  ));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key))))
+  );
   self.clients.claim();
 });
 
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request).catch(() => caches.match(OFFLINE_URL)))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request)
+        .then(networkResponse => {
+          if(event.request.url.startsWith("http")){
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, networkResponse.clone()));
+          }
+          return networkResponse;
+        })
+        .catch(()=>caches.match(OFFLINE_URL));
+    })
   );
 });
