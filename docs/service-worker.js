20888f1db90812a0ee9e9cc8c676404d3594c4a5
words1941-commits <words1941@gmail.com>
2025-11-13 12:19:16 +0200

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'zimdocs-cache-v3';
+const CACHE_NAME = 'zimdocs-pro-v3';
 const FILES_TO_CACHE = [
   './',
   './index.html',
+  './manifest.json',
   './css/styles.css',
   './js/app.js',
-  './manifest.json',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/maskable.png',
@@ -12,30 +12,24 @@ const FILES_TO_CACHE = [
   './icons/screenshot2.png'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => key !== CACHE_NAME && caches.delete(key))
+    ))
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
